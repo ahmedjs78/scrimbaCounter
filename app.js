b449cb56c824f265ec1431fa1452a6326d0e2f58
ahmed mahmood <ahmedjs778@gmail.com>
2024-02-11 13:50:07 +0400

    n

---
+++ app.js
@@ -0,0 +1,29 @@
+let counter = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let newCountEl = document.getElementById("newCEl")
+let amte = document.getElementById("not")
+let count = 0
+
+
+function increment(){
+    count += 1
+    console.log(count)
+    counter.textContent = count
+}
+
+
+function save(){
+    if( count === 0){
+        amte.textContent = "nothing to save"
+    }else{
+        let data = count + " - "
+        saveEl.textContent += data
+        count = 0
+        counter.textContent = count
+        amte.textContent = ""
+    }
+};
+
+function newc (){
+    saveEl.textContent = "Previous entries:"
+}
